466895d1bafe9b02d236bae7974530dc7d30791b
Ellie <112726692+elliezub@users.noreply.github.com>
2023-12-02 21:46:08 -0400

    add part2 question

---
+++ 2023/day-2/day-2b.py
@@ -0,0 +1,56 @@
+
+# part 2 - in progress
+# For each game, find the minimum set of cubes that must have been present. 
+# What is the sum of the power of these sets?
+
+# The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
+import re
+
+puzzle_input = open("puzzle-input.txt", "r")
+
+organized_games = {}
+
+possible_games_sum = 0
+game_number = 0
+max_red = 12
+max_green = 13
+max_blue = 14
+
+for line in puzzle_input:
+    game_number += 1
+    individual_rounds = []
+    rounds_data = line.strip().split(';')
+
+    for round_data in rounds_data:
+        round_dict = {}
+        # get the color/count pairs for each round
+        pairs = re.findall(r'(\d+)\s*(red|green|blue)', round_data)
+          
+        # add color/count pairs to round dict
+        for count, color in pairs:
+            round_dict[color] = int(count)
+        
+        individual_rounds.append(round_dict)
+
+    organized_games[game_number] = individual_rounds # add colors and counts here
+
+for game_num in organized_games:
+    for game in organized_games[game_num]:
+        game_possible = True
+        
+        for color in game:
+            if (color == "red" and game[color] > max_red) or \
+               (color == "green" and game[color] > max_green) or \
+               (color == 'blue' and game[color] > max_blue):
+                game_possible = False
+                break 
+
+        if not game_possible:
+            break
+
+    if game_possible == True:
+        possible_games_sum += game_num
+    
+print(possible_games_sum)
+
+puzzle_input.close()
